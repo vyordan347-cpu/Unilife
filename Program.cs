f732322f3e275df0b1e2edd4790719be44608293
vyordan347 <vyordan347@gmail.com>
2026-04-26 23:09:20 -0500

    Ejecuta migraciones automaticamente al iniciar

--- Program.cs
+++ Program.cs
@@ -5,19 +5,18 @@ using Unilife.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// MVC
+
 builder.Services.AddControllersWithViews();
 
-// Base de datos SQLite
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-// Identity + Roles
 builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>()
     .AddDefaultTokenProviders();
 
-// Configurar ruta del login
+
 builder.Services.ConfigureApplicationCookie(options =>
 {
     options.LoginPath = "/Account/Login";
@@ -26,7 +25,6 @@ builder.Services.ConfigureApplicationCookie(options =>
 
 var app = builder.Build();
 
-// Pipeline HTTP
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
@@ -38,19 +36,22 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
-// Primero Authentication, luego Authorization
 app.UseAuthentication();
 app.UseAuthorization();
 
-// Ruta por defecto
+
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Account}/{action=Login}/{id?}");
 
-// Crear roles y usuario coordinador inicial
+
 using (var scope = app.Services.CreateScope())
 {
     var services = scope.ServiceProvider;
+
+    var context = services.GetRequiredService<ApplicationDbContext>();
+    await context.Database.MigrateAsync();
+
     await SeedData.InicializarAsync(services);
 }
 
